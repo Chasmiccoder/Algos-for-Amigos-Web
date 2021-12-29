31ecff41a5a399ea08869b253a4a4339d9e9d100
Aryaman Kolhe <chasmiccoder@gmail.com>
2021-12-29 14:24:06 +0530

    successful box swap animation

--- js/app.js
+++ js/app.js
@@ -21,3 +21,52 @@ var playPause = anime({
 
 document.querySelector('#boxes .play').onclick = playPause.play;
 document.querySelector('#boxes .pause').onclick = playPause.pause;
+
+document.querySelector('#swapBox').onclick = swap;
+
+// prints the coordinates of the red box
+function getRedBoxPos() {
+    var rect = document.getElementById('redBox').getBoundingClientRect();
+    console.log(rect.top, rect.right, rect.bottom, rect.left);
+}
+
+function swap() {
+    var rect1 = document.getElementById('redBox').getBoundingClientRect();
+    var rect2 = document.getElementById('blueBox').getBoundingClientRect();
+    
+    console.log("RB:", rect1.top, rect1.right, rect1.bottom, rect1.left);
+    console.log("BB:", rect2.top, rect2.right, rect2.bottom, rect2.left);
+
+    //Output
+    //RB: 104.66667175292969 498 204.6666717529297 398
+    //BB: 104.66667175292969 610 204.6666717529297 510
+    
+    let x = rect2.right - rect1.right;
+
+    var swapAnim1 = anime({
+        targets: '#redBox',
+        translateY: [
+            {value: 100, duration: 1000}, // duration is in milliseconds
+            {value: 0, duration: 1000}
+        ],
+        translateX: [
+            {value: x, duration: 1000}
+        ],
+    });
+
+    var swapAnim2 = anime({
+        targets: '#blueBox',
+        translateY: [
+            {value: 100, duration: 1000}, // duration is in milliseconds
+            {value: 0, duration: 1000}
+        ],
+        translateX: [
+            {value: -x, duration: 500}
+        ],
+    });
+
+    // swapAnim1.play();
+    // swapAnim2.play();
+}
+
+
