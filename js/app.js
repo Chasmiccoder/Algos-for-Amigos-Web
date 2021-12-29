b87068daa418ceaba384e7580ca12992dc6f8685
Aryaman Kolhe <chasmiccoder@gmail.com>
2021-12-29 16:41:27 +0530

    Array + Single Swap implemented

--- js/app.js
+++ js/app.js
@@ -1,72 +1,119 @@
 const anime = require('animejs');
+var main_array = new Array();
 
-var playPause = anime({
-    targets: 'div.box',
-    translateY: [
-        {value: 200, duration: 500}, // duration is in milliseconds
-        {value: 0, duration: 800}
-    ],
-    rotate: {
-        value: '1turn',
-        easing: 'easeInOutSine'
-    },
-    // backgroundColor: '#505050', // can use normal css stuff here as well
-    opacity: '0.3',
-    delay: function(element, iteration, time) {
-        return iteration * 1000;
-    },
-    autoplay: false,
-    loop: true
-});
+document.getElementById('insertButton').onclick = insertElement;
 
-document.querySelector('#boxes .play').onclick = playPause.play;
-document.querySelector('#boxes .pause').onclick = playPause.pause;
+// inserts a number into the array
+function insertElement() {
+    let num = document.getElementById('insertInput').value;
+    document.getElementById('insertInput').value = "";  // clear the old value
 
-document.querySelector('#swapBox').onclick = swap;
+    if(isNaN(parseInt(num))) {
+        console.log("Input element is not a number!");
+        return;
+    }
 
-// prints the coordinates of the red box
-function getRedBoxPos() {
-    var rect = document.getElementById('redBox').getBoundingClientRect();
-    console.log(rect.top, rect.right, rect.bottom, rect.left);
+    if(main_array.length == 5) {
+        console.log("temporary array size limitation");
+    } else {
+        main_array.push(parseInt(num));
+    }
+
+    updateArray();
+    
+    // console.log(main_array);
 }
 
+// updates the array frontend
+function updateArray() {
+    let mainArray = document.getElementById('mainArray');
+
+    // clear the array
+    while(mainArray.firstChild) {
+        mainArray.removeChild(mainArray.lastChild);
+    }
+
+    for(let i = 0; i < main_array.length; i++) {
+    
+        let array_element = document.createElement('div');
+        array_element.setAttribute('id', 'mainArrayElement_' + i.toString());
+        array_element.setAttribute('style', 'background-color: #CBC3E3; height: 50px; width: 50px; position: relative; margin: 5px; display: inline-block; text-align: center; vertical-align: middle; line-height: 50px;');
+        array_element.innerHTML = main_array[i].toString();
+        mainArray.appendChild(array_element);
+        
+    }
+
+    console.log(main_array);
+
+}
+
+document.querySelector('#sortButton').onclick = swap;
+
+// swaps ith and jth blocks
+// later, use i and j as arguments for this func
 function swap() {
-    var rect1 = document.getElementById('redBox').getBoundingClientRect();
-    var rect2 = document.getElementById('blueBox').getBoundingClientRect();
-    
-    console.log("RB:", rect1.top, rect1.right, rect1.bottom, rect1.left);
-    console.log("BB:", rect2.top, rect2.right, rect2.bottom, rect2.left);
+    let i = 0;
+    let j = 1;
 
-    //Output
-    //RB: 104.66667175292969 498 204.6666717529297 398
-    //BB: 104.66667175292969 610 204.6666717529297 510
-    
-    let x = rect2.right - rect1.right;
+    let e1 = 'mainArrayElement_' + i.toString();
+    let e2 = 'mainArrayElement_' + j.toString()
 
-    var swapAnim1 = anime({
-        targets: '#redBox',
+    let rect1 = document.getElementById(e1).getBoundingClientRect();
+    let rect2 = document.getElementById(e2).getBoundingClientRect();
+
+    // quick fix for when j's x value is lesser than i's
+    if(rect2.right - rect1.right < 0) {
+        let tmp = i;
+        i = j;
+        j = tmp;
+        
+        let tmp2 = e2;
+        e2 = e1;
+        e1 = tmp2;
+
+        let tmp3 = rect1;
+        rect1 = rect2;
+        rect2 = tmp3;
+    }
+
+    
+    // rough implementation of changing the array after the animation has happened
+    // FIXME
+    doAnim(function() {
+        doUpdate();
+    });
+
+    function doAnim(e1, e2, distance, callback) {
+        let distance = rect2.right - rect1.right;
+        moveElement(e1, distance);
+        moveElement(e2, -distance);
+        callback();
+    }
+
+    function doUpdate() {
+        // let tmp = main_array[i];
+        // main_array[i] = main_array[j];
+        // main_array[j] = tmp;
+        // updateArray();
+    }
+}
+
+// moves the element along the x axis by the given distance
+function moveElement(elementId, d) {
+
+    console.log(elementId,d);
+
+    let moveAnimation = anime({
+        targets: '#' + elementId,
         translateY: [
             {value: 100, duration: 1000}, // duration is in milliseconds
             {value: 0, duration: 1000}
         ],
         translateX: [
-            {value: x, duration: 1000}
+            {value: d, duration: 1000}
         ],
+        easing: 'easeInOutExpo'
     });
-
-    var swapAnim2 = anime({
-        targets: '#blueBox',
-        translateY: [
-            {value: 100, duration: 1000}, // duration is in milliseconds
-            {value: 0, duration: 1000}
-        ],
-        translateX: [
-            {value: -x, duration: 500}
-        ],
-    });
-
-    // swapAnim1.play();
-    // swapAnim2.play();
 }
 
 
